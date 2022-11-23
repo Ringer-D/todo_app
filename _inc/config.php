ea214ab9f9909f5a7595a531c4f351d86de49536
Ringer-D <dusan.ringer@gmail.com>
2022-11-23 19:06:19 +0100

    connecting to database

--- _inc/config.php
+++ _inc/config.php
@@ -5,3 +5,27 @@
 ini_set('display_startup_errors', 'On');
 ini_set('display_errors', 'On');
 error_reporting(-1);
+
+// require
+
+require_once 'vendor/autoload.php';
+
+use Medoo\Medoo;
+
+// connect to DB
+
+$database = new Medoo([
+	// [required]
+	'type' => 'mysql',
+	'host' => 'localhost',
+	'database' => 'todo',
+	'username' => 'root',
+	'password' => 'root',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_general_ci',
+
+]);
+
+echo '<pre>';
+print_r( $database->info() );
+echo '</pre>';
