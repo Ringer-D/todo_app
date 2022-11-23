efe211548505318cbc24572daa14c05caf54e432
Ringer-D <dusan.ringer@gmail.com>
2022-11-23 21:33:03 +0100

    add item to database

--- _inc/add-new.php
+++ _inc/add-new.php
@@ -1,1 +1,18 @@
-<?php echo $_GET['message']; ?>
+<?php 
+
+// include
+
+require 'config.php';
+
+// add new item
+
+$id = $database -> insert( 'items', [
+    'text' => $_POST['message']
+] );
+
+if( $id ) {
+    echo 'SENDED FUCK YEAH !'?><br><?php ;
+    $html = '<a href="/todo_app">back home</a>';
+    echo $html;
+}
+
