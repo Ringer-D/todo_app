9c2c7145dfd3888e56067329c139413a12bb4be2
Ringer-D <dusan.ringer@gmail.com>
2022-12-05 18:27:07 +0100

    create edit & delete link

--- _partials/header.php
+++ _partials/header.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,300,0,0" />
     <link href="assets/css/style.css" rel="stylesheet">
     <title>Todo-App</title>
 </head>
--- index.php
+++ index.php
@@ -4,13 +4,19 @@
     <h1>v3ry gOOd TODO APP</h1>
 </div>
 
-<?php $data = $database -> select( 'items', 'text' ); ?>
+<?php $data = $database -> select( 'items', [ 'id', 'text' ] ); ?>
 
 <div class="container-main container flex flex-row justify-center items-start">
     <ul class="list-group text-white w-96 mr-10">
         <?php
             foreach( $data as $item ) {
-                echo '<li class="p-1 pl-2 mb-1 border border-white/[.20] rounded">' . $item . '</li>';
+                echo '<li class="p-1 pl-2 mb-1 flex flex-row justify-between items-center border border-white/[.20] rounded">';
+                echo    $item[ 'text' ];
+                echo    '<div class="control flex flex-row items-center">';
+                echo        '<a href="edit.php?id='. $item[ 'id' ] .'" class="edit-link mr-2 p-1 text-sm text-green-700 hover:font-medium no-underline">edit</a>';
+                echo        '<a href="delete.php?id='. $item[ 'id' ] .'" class="delete-link p-1 text-gray-400 hover:text-red-500 relative top-0.5 no-underline"><span class="material-symbols-outlined">delete</span></a>';
+                echo    '</div>';
+                echo '</li>';
             }
         ?>
     </ul>
